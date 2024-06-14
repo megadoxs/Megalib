e2c8da69b16a95daed0c0d2b83b1cea5fcf6af76
Louis Caron <megadoxs@gmail.com>
2024-06-13 22:38:23 -0400

    init

--- src/client/java/com/megadoxs/megalib/MegalibClient.java
+++ src/client/java/com/megadoxs/megalib/MegalibClient.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class MegalibClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		MegalibClientPowers.register();
 	}
 }
