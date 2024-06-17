f14c0b332af6d015ec36f234ed5bcf38164a8891
Louis Caron <megadoxs@gmail.com>
2024-06-16 21:52:35 -0400

    trying to figure out how to test powers

--- src/main/java/com/megadoxs/megalib/Megalib.java
+++ src/main/java/com/megadoxs/megalib/Megalib.java
@@ -13,16 +13,13 @@ public class Megalib implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 
 	public static final String MOD_ID = "megalib";
-    public static final Logger LOGGER = LoggerFactory.getLogger("megalib");
+    public static final Logger LOGGER = LoggerFactory.getLogger("Megalib.class");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 		MegalibPowers.register();
+
+		LOGGER.info("Megalib has initialized. Ready to power up your game!");
 	}
 	public static Identifier identifier(String path) {
 		return new Identifier(MOD_ID, path);
